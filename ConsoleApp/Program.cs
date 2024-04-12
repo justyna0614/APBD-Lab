3260bb10b661f40e46910e44e2ae2f26a387e0a9
s25802 <s25802@pjwstk.edu.pl>
2024-04-12 20:21:21 +0200

    Modyfikacja 3

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,5 +6,6 @@
         String name = Console.ReadLine()!;
         Console.WriteLine($"Hello, {name}! Enter your lastname:");
         String lastName = Console.ReadLine()!;
+        Console.WriteLine($"Hello, {name} {lastName}!");
     }
 }

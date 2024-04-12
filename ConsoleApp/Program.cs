fe1347b5684c8f73da08d0023ec9140a5fdc8516
s25802 <s25802@pjwstk.edu.pl>
2024-04-12 20:34:14 +0200

    AVG method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,5 +7,19 @@
         Console.WriteLine($"Hello, {name}! Enter your lastname:");
         String lastName = Console.ReadLine()!;
         Console.WriteLine($"Hello, {name} {lastName}!");
+        int[] numbers = { 2, 6, 4 };
+        var avg = Avg(numbers);
+        Console.WriteLine($"AVG= {avg}");
+    }
+
+    private static int Avg(int[] arr)
+    {
+        var sum = 0;
+        var size = arr.Length;
+        foreach (var number in arr)
+        {
+            sum += number;
+        }
+        return sum / size;
     }
 }

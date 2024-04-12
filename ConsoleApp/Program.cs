8d89249bbe0a0d1c9278231a1f14c670610caa3d
s25802 <s25802@pjwstk.edu.pl>
2024-04-12 20:10:26 +0200

    Modyfikacja 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine()!;
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
